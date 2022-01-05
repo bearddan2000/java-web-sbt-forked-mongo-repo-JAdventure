203e499d75a7902a42ed1ce4936c066117d84633
bearddan2000 <bearddan2000@yahoo.com>
2022-01-04 20:27:49 -0500

    initial commit

---
+++ api/bin/src/main/java/example/service/WeaponServiceImpl.java
@@ -0,0 +1,34 @@
+package example.service;
+
+import example.model.Weapon;
+import example.repository.WeaponRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author ragcrix
+ */
+@Service
+public class WeaponServiceImpl implements WeaponService {
+
+    @Autowired
+    private WeaponRepository weaponRepository;
+
+    @Override
+    public List<Weapon> findAll() {
+        return weaponRepository.findAll();
+    }
+
+    @Override
+    public Weapon findByName(String name) {
+        List<Weapon> lst = weaponRepository.findAll();
+        for(Weapon x : lst)
+          if(x.getName().equals(name))
+            return x;
+
+        return null;
+    }
+}
